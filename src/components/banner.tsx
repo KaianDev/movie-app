a91c319e0e64dfd63ccae96e1b54ef317cebd3bd
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-06-02 14:54:31 -0300

    style: add resposiveness to Banner and Footer component

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,7 +1,7 @@
 export const Banner = () => {
   return (
-    <section className="h-[510px] bg-[url('/bg-2.png')] bg-cover flex items-center justify-center flex-col gap-3">
-      <h2 className="max-w-[530px] text-center text-white text-4xl font-semibold">
+    <section className="h-[510px] bg-[url('/bg-2.png')] bg-cover flex items-center justify-center flex-col gap-3 p-5">
+      <h2 className="max-w-[530px] text-center text-white  text-3xl sm:text-4xl font-semibold">
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
       </h2>
       <p className="max-w-[420px] text-sm text-white/80 text-center">
